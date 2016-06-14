bb5dc280ce65ab3b9530e36fadaf9fca8dcb7311
Mandeep Singh <mandeep@courses.ninja>
2016-06-14 12:48:54 +0530

    updated code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express  = require('express'),
     // Mongoose Schema definition
 //Update user model (todo)
     Schema = new mongoose.Schema({
-      studentId       : String, 
+      firstName       : String,
       name    : String,
       department: String,
       year    : String ,   
@@ -40,10 +40,13 @@ app.get('/', function (req, res) {
             console.log(err)
         }
     });
-
 });
 
-  app.post('/api/user', function (req, res) {
+app.get('/adduser', function(req, res){
+    res.render('addUser.ejs')
+})
+
+  app.post('/api/adduser', function (req, res) {
 
       //update post function as per new model (todo)
         var user = new User(
@@ -59,8 +62,8 @@ app.get('/', function (req, res) {
     // http://mongoosejs.com/docs/api.html#model_Model-save
     user.save(function (err, data) {
         if(!err && data){
-            console.log(data);
-            res.status(200).json(data)
+            console.log('Data added successfully');
+            res.redirect('/')
         } else {
             res.json(500, {msg: 'Something went wrong' });
             console.log(err)
@@ -80,29 +83,49 @@ app.get('/', function (req, res) {
     });
   })
 
-  app.get('/api/users/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
+app.get('/userdetails/:id', function(req, res){
     User.findById( req.params.id, function ( err, user ) {
         if(!err && user){
-            res.status(200).json(user)
+            res.render('userDetail.ejs',{
+                data : user
+            })
         } else {
             console.log(err)
         }
     });
-  })
+} )
 
-  app.put('/api/users/:id', function (req, res) {
+  // app.get('/api/users/:id', function (req, res) {
+  //   // http://mongoosejs.com/docs/api.html#model_Model.findById
+  //
+  // })
+
+
+app.get('/edituser/:id', function(req, res){
+    User.findById( req.params.id, function ( err, user ) {
+        if(!err && user){
+            res.render('editUser.ejs',{
+                data : user
+            })
+        } else {
+            console.log(err)
+        }
+    });
+
+})
+
+  app.put('/api/edituser/:id', function (req, res) {
     // http://mongoosejs.com/docs/api.html#model_Model.findById
     User.findById( req.params.id, function ( err, user ) {
-      user.isPassedOut = req.body.completed;
+      user.department = req.body.department;
       // http://mongoosejs.com/docs/api.html#model_Model-save
       user.save( function ( err, data ){
           if(!err && data){
-           res.status(200).json(data)
+          res.redirect('/')
           } else {
               console.log(err)
           }
-       
+
       });
     });
   });
